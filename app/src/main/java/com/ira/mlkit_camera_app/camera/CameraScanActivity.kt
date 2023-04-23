a10e6505442e5b61036ea2b5078c0c1bcef4c4ad
qwert1234-ai <81895294+qwert1234-ai@users.noreply.github.com>
2023-04-23 15:31:24 +0600

    23.04

--- app/src/main/java/com/ira/mlkit_camera_app/camera/CameraScanActivity.kt
+++ app/src/main/java/com/ira/mlkit_camera_app/camera/CameraScanActivity.kt
@@ -3,11 +3,24 @@ package com.ira.mlkit_camera_app.camera
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.ira.mlkit_camera_app.R
 
-class CameraScanActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class CameraScanActivity : AppCompatActivity() {
+    private lateinit var cameraFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+
+        cameraFuture = ProcessCameraProvider.getInstance(this)
+        cameraFuture.addListener({
+
+
+
+            }, ContextCompat.getMainExecutor(this))
     }
 }
--- app/src/main/java/com/ira/mlkit_camera_app/startScan/ScanFragment.kt
+++ app/src/main/java/com/ira/mlkit_camera_app/startScan/ScanFragment.kt
@@ -1,14 +1,37 @@
 package com.ira.mlkit_camera_app.startScan
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+
 import com.ira.mlkit_camera_app.R
+import com.ira.mlkit_camera_app.camera.CameraScanActivity
 
 class ScanFragment : Fragment(){
+    private  val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        {isGranted:Boolean ->
+            if(isGranted == true){
+                startScan()
+            } else {
+                //домашнее задание. Скрыть кнопку и показаать сообщение.
+            }
+        }
+    )
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,8 +43,21 @@ class ScanFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val startButton = view.findViewById<Button>(R.id.scan_button)
+        //startButton.isVisible
         startButton.setOnClickListener {
-            
+            val permission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            when (permission) {
+                PackageManager.PERMISSION_GRANTED -> {
+                    startScan()
+                }
+                else -> {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
         }
+    }
+    private fun startScan() {
+        val intent = Intent(requireContext(), CameraScanActivity::class.java)
+        startActivity(intent)
     }
 }
